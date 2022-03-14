fcdc5274d7d12fd03e060a873a8cb1d08c6884f8
ujwaladhangar <ujwaladhangar037@gmail.com>
2022-03-14 14:01:06 +0530

    ADDED: Line Comparison Problem

---
+++ src/com/infogalaxy/linecomparisonproblem/LineComparisonProblem.java
@@ -0,0 +1,9 @@
+package com.infogalaxy.linecomparisonproblem;
+
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+
+        //Welcome Message For User
+        System.out.println("Welcome to Line Comparison Computation Problem Developed by Ujwala Dhangar. ");
+    }
+}
